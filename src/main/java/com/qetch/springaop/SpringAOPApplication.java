5d81fea2af4abb529369041ea9a617a0236e5814
JiaFeiCat93 <1046709362@qq.com>
2018-12-28 11:53:48 +0800

    update project name

--- src/main/java/com/qetch/springaop/SpringAOPApplication.java
+++ src/main/java/com/qetch/springaop/SpringAOPApplication.java
@@ -1,20 +1,25 @@
 package com.qetch.springaop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class SpringAOPApplication extends SpringBootServletInitializer {
+public class SpringAOPApplication { // extends SpringBootServletInitializer - used with external Tomcat
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringAOPApplication.class, args);
 	}
 
-	@Override
+	/**
+	 * The following code is needed to be used with external Tomcat.
+	 */
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringAOPApplication.class);
-	}
+	}*/
 	
 }
